7a27d030d64ed72857cc1b7406cd9e3eb5b4e2a0
zzeerr000 <nikita20040617@gmail.com>
2026-03-18 17:09:03 +0300

    gitignoreupdate

--- backend/node_modules/@nestjs/passport/dist/interfaces/type.interface.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Type<T = any> extends Function {
-    new (...args: any[]): T;
-}
-export type WithoutCallback<T extends any[]> = T extends any ? T extends [...infer U, infer C] ? C extends Function ? U : T : T : never;
